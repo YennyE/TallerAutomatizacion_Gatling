bcd44399cd1fefb14644f0dc05f8623e5b5c4b79
YennyE <44005339+YennyE@users.noreply.github.com>
2025-06-24 21:27:16 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -25,6 +25,6 @@ class LoginTest extends Simulation{
 
   // 3 Load Scenario
   setUp(
-    scn.inject(rampUsersPerSec(5).to(15).during(30))
+    scn.inject(rampUsersPerSec(4).to(15).during(30))
   ).protocols(httpConf);
 }
